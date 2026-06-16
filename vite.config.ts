c8c18c6440ed6f27f529811beee1ba1fd8f66443
TausifSS <Tausifss015@gmail.com>
2026-06-16 17:29:45 +0530

    Update base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/velixapp/',
+  base: '/velix/',
   plugins: [
     react(),
     tailwindcss(),
